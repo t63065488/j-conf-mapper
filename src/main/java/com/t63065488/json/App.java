7cb7befe768449f9d6d2b0cb1ee38cd07f71f1dd
t63065488 <t63065488@gmail.com>
2024-04-12 19:20:58 +0100

    mvn spotless:apply/config

--- src/main/java/com/t63065488/json/App.java
+++ src/main/java/com/t63065488/json/App.java
@@ -4,10 +4,8 @@ package com.t63065488.json;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
--- src/test/java/com/t63065488/json/AppTest.java
+++ src/test/java/com/t63065488/json/AppTest.java
@@ -1,20 +1,18 @@
 package com.t63065488.json;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
 }
